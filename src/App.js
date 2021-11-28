482183e6308fe20b2e4d1be782aa6270dc3f0ff7
Alejandro Benitez <alejandrobenitezcorp@outlook.com>
2021-11-27 22:20:07 -0300

    Se realiza la conexión con API mediante useEffect y axios, se importa bootstrap, se crean componentes de tabla, se llaman todas las crypto-monedas en columna name de la tabla

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import TableCoins from "./components/TableCoins";
 
 function App() {
-  return <div className="App"></div>;
+  const [coins, setCoins] = useState([]);
+
+  const getData = async () => {
+    const res = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    console.log(res.data);
+    setCoins(res.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Foro Crypto</h1>
+      <TableCoins coins={coins} />
+    </div>
+  );
 }
 
 export default App;
